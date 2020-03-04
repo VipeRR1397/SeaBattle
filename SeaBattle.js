77e7852b643845372e65006b5a5ccd2a826df229
VipeRR1397 <59103379+VipeRR1397@users.noreply.github.com>
2020-03-04 17:47:53 +0300

    Add files via upload

--- SeaBattle.js
+++ SeaBattle.js
@@ -15,6 +15,10 @@ const play = {
     shots: 0,
     misses: 0,
     kills: 0,
+    set updateData(data) {
+        this[data] += 1;
+        this.render();
+    },
     render() {
         shots.textContent = this.shots;
         misses.textContent = this.misses;
@@ -39,15 +43,14 @@ const show = {
     }
 }
 
-
 const fire = (event) => {
-    const target = event.target;
-    show.misses(target);    
-        play.shots += 1;
-        play.render();
-    if (target.classList.contains('misses')){
-        return 0;
-    }
+    
+    const target = event.target; 
+    if (target.classList.contains('misses')) {
+        return;
+    };
+    show.misses(target);
+    play.updateData = 'shots';
 
     console.log(target, typeof(target));
     }; 
